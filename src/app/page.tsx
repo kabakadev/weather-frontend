4b54e87db4486f8368b948bafe5d37b28c7938e9
Ian Kabaka <iankabaka1@gmail.com>
2025-04-22 19:59:02 +0300

    fetching data from the backend api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
 import Header from "./components/Header";
 import CurrentWeather from "./components/CurrentWeather";
+import { useState } from "react";
 export default function Home() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleSearch = async (city: string, unit: "C" | "F") => {
+    try {
+      const response = await fetch(
+        `http://localhost:8000/api/weather?city=${city}&unit=${unit}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Failed to fetch weather:", error);
+    }
+  };
   return (
     <main className="min-h-screen bg-gray-100 p-6">
-      <Header />
+      {/* <Header /> */}
       <div className="flex flex-col md:flex-row gap-6">
         {/* Left panel - Current Weather */}
         <div className="w-full md:w-1/3">
